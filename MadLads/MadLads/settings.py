aff6c99816dd31a8e0633ad0e20628db7938d4cf
Kody <kodyneill@gmail.com>
2019-11-12 09:31:23 -0700

    Fixed the fix that I thought I fixed :(

--- MadLads/MadLads/settings.py
+++ MadLads/MadLads/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'MadLads.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['MadLads/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
