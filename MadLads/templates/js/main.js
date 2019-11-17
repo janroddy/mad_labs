528b2f8ff846580e71463d792dc7dd71cf239391
janroddy <jan.roddy.e@gmail.com>
2019-11-16 17:22:33 -0700

    add dynamic form capability

--- MadLads/templates/js/main.js
+++ MadLads/templates/js/main.js
@@ -1,55 +1,54 @@
-$(function() {
 
-//returns array of user input
-function captureInput() {
-  var userInput = [];
-  var madStory = "One morning, about a week after Bingley's engagement with $noun$ had been formed, as he and the females of the family were sitting together in the dining-room, their attention was suddenly drawn to the window, by the sound of a carriage; and they perceived a chaise and four driving up the lawn."
-
-  	// hide the story from view
+//This function runs as soon as the page loads
+$(document).ready(function() {
 	$("#story").hide();
+	produceInputForms(arr);
 
-	// ---- event handler ---- //
-	$("#btn-click").click(function(e) {		
-		
-		// grab the values from the input boxes, then append them to the DOM
-		$(".box1").empty().append($("input.box1").val());
-		userInput.push(($("input.box1").val()));
+});
 
-		$(".box2").empty().append($("input.box2").val());
-		userInput.push(($("input.box2").val()));
+//On the button click save the user input and display the story 
+$("#btn-click").click(function(e) {		
+	for (var i = 0; i < arr.length; i++) {
+		var input = $("#box" + i).val();
+		console.log(input)
 
-		$(".box3").empty().append($("input.box3").val());
-		userInput.push(($("input.box3").val()));
-		
+	//add input to userInput array	
+		userInput.push(input);
 
-		// $(".story").empty().append(story);
-		// show the story
-		
-		$("#mad-story").empty().append(madStory);
-		$("#story").show();
+	}
+	console.log(userInput[0]);
 
-		// empty the form's values
-		$(':input').val('');
-
-		// hide the questions
-		$("#questions").hide();
+	$("#in0").empty().append($(userInput[0]));
+	
+	// append the story to the DOM
+	$("#mad-story").empty().append(madStory);
+	//show the story to the user
+	$("#story").show();
+	// empty the form's values	
+	$(':input').val('');
+	// hide the questions
+	$("#questions").hide();
 		
 
 	});
+//global variable to store the userInput      
+userInput = [];
+//sample array and story for testing purposes 
+arr = ["noun", "verb", "adjective", "name"]
+var madStory = "One morning, about a week after Bingley's engagement with <span id='in0'></span> had been formed, as he and the females of the family were sitting together in the dining-room, their attention was suddenly drawn to the window, by the sound of a carriage; and they perceived a chaise and four driving up the lawn."
 
-  return userInput;
+//arr is am array with they number and types of words for the user to input
+function produceInputForms(arr) {
+	for (var i = 0; i < arr.length; i++) {
+		$("form").append("<label for='box" + i + "'> Enter a " + arr[i] + "</label>" + "<br>");
+		$("form").append("<input id='box" + i + "'>" + "<br>");
+	}
+	/*
+	arr.forEach(element => $("form").append("<label for=box" + count + "> Enter a " + element + "</label>" + "<br>" + "<input>" + "<br>" 
+		));
+		*/	
+       
 }
 
 
 
-
-var userIn = captureInput();
-console.log(userIn)
-
-
-	$("#play-btn").click(function(e) {
-		$("#questions").show();
-		$("#story").hide();
-	});
-
-});
