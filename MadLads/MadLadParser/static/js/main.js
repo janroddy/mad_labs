557bdfa87ea0286ded881ef3039792b0cd1ce42f
Kody <kodyneill@gmail.com>
2019-11-21 17:43:07 -0700

    Bleh

--- MadLads/MadLadParser/static/js/main.js
+++ MadLads/MadLadParser/static/js/main.js
@@ -4,54 +4,53 @@ $(document).ready(function() {
 	$("#story-title").empty().append(storyName);
 
 	$("#story").hide();
-	produceInputForms(arr);
+	//produceInputForms(arr);
 
 });
 
-//On the button click save the user input and display the story 
-$("#btn-click").click(function(e) {		
+//On the button click save the user input and display the story
+$("#btn-click").click(function(e) {
 	for (var i = 0; i < arr.length; i++) {
 		var input = $("#box" + i).val();
 		console.log(input)
 
-	//add input to userInput array	
+	//add input to userInput array
 		userInput.push(input);
 
 	}
 	console.log(userInput[0]);
 
 	$("#in0").empty().append($(userInput[0]));
-	
+
 	// append the story to the DOM
 	$("#mad-story").empty().append(madStory);
 	//show the story to the user
 	$("#story").show();
-	// empty the form's values	
+	// empty the form's values
 	$(':input').val('');
 	// hide the questions
 	$("#questions").hide();
-		
+
 
 	});
-//global variable to store the userInput      
+//global variable to store the userInput
 userInput = [];
-//sample array and story for testing purposes 
+//sample array and story for testing purposes
 var storyName = "Mad Pride and Prejudice"
 arr = ["noun", "verb", "adjective", "name"]
 var madStory = "One morning, about a week after Bingley's engagement with <span id='in0'></span> had been formed, as he and the females of the family were sitting together in the dining-room, their attention was suddenly drawn to the window, by the sound of a carriage; and they perceived a chaise and four driving up the lawn."
 
 //arr is am array with they number and types of words for the user to input
 function produceInputForms(arr) {
-	for (var i = 0; i < arr.length; i++) {
-		$("form").append("<label for='box" + i + "'> Enter a " + arr[i] + "</label>" + "<br>");
-		$("form").append("<input id='box" + i + "'>" + "<br>");
+	var count = 0;
+	for (word in arr) {
+		$("form").append("<label for='box" + count + "'> Enter a " + word + "</label>" + "<br>");
+		$("form").append("<input id='box" + count + "'>" + "<br>");
+		count++;
 	}
 	/*
-	arr.forEach(element => $("form").append("<label for=box" + count + "> Enter a " + element + "</label>" + "<br>" + "<input>" + "<br>" 
+	arr.forEach(element => $("form").append("<label for=box" + count + "> Enter a " + element + "</label>" + "<br>" + "<input>" + "<br>"
 		));
-		*/	
-       
+		*/
+
 }
-
-
-
--- MadLads/MadLadParser/views.py
+++ MadLads/MadLadParser/views.py
@@ -5,6 +5,7 @@ from .models import Story
 # Create your views here.
 def display_home(request):
     return render(request, 'index.html')
+
 def display_about(request):
     return render(request, 'about.html')
 
@@ -15,4 +16,6 @@ def display_howToPlay(request):
     return render(request, 'how-to-play.html')
 
 def display_story(request):
-    return render(request, 'story.html')
+
+    story = {"noun,verb,adjective,name"}
+    return render(request, 'story.html', {'Story' : story})
