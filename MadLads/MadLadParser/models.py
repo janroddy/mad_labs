f9d8166dd01ec606fe4dc2bf028d0bf642ab2197
Kody <kodyneill@gmail.com>
2019-12-05 10:26:30 -0700

    FULL SAVE GOD SPEED

--- MadLads/MadLadParser/models.py
+++ MadLads/MadLadParser/models.py
@@ -6,7 +6,7 @@ class Story(models.Model):
     story_name = models.CharField(max_length = 200, default=None, blank=True)
     story_text = models.CharField(max_length = 1500, default=None, blank=True)
     story_array = models.CharField(max_length = 1500, default=None, blank=True)
-    #story_category = models.CharField(max_length=200, default=None, blank=True)
+    story_category = models.CharField(max_length=200, default=None, blank=True)
 
     def __str__(self):
         return self.story_name + ", " + "%d" % self.pk
